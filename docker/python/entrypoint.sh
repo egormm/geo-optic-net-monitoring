620dac92ef432055a50060cc1dcc0befb35266b5
egormm <egormm@gmail.com>
2020-04-16 13:30:44 +0300

    rearrange entrypoint script

--- docker/python/entrypoint.sh
+++ docker/python/entrypoint.sh
@@ -5,12 +5,12 @@ python manage.py migrate
 
 CONTAINER_ALREADY_STARTED=".CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    touch $CONTAINER_ALREADY_STARTED
-    echo "-- First container startup --"
-    python manage.py create_custom_admin --email $DJANGO_ADMIN_EMAIL --password $DJANGO_ADMIN_PASS
-    python manage.py load_clients --filename=clients.csv
+  touch $CONTAINER_ALREADY_STARTED
+  echo "-- First container startup --"
+  python manage.py create_custom_admin --email $DJANGO_ADMIN_EMAIL --password $DJANGO_ADMIN_PASS
+  python manage.py load_clients --filename=clients.csv
 else
-    echo "-- Not first container startup --"
+  echo "-- Not first container startup --"
 fi
 
 python manage.py collectstatic --noinput
